fc669ed7ee45873fa1c8060a5315c3b9b64e7433
CadenceReynolds <reynocal001@warren.k12.in.us>
2024-10-31 12:22:07 -0400

    Finishes Assignment

--- src/Main.kt
+++ src/Main.kt
@@ -1,2 +1,9 @@
-class Main {
+fun main() {
+    val newBuildIdea = mutableListOf(
+        "Stone", "Stone_Bricks", "Pale_Planks", "Pale_Logs"
+    )
+    newBuildIdea.add(2, "Birch_Stripped_Logs")
+    newBuildIdea[2] = "Birch_Planks"
+    newBuildIdea.remove("Birch_Planks")
+    println(newBuildIdea)
 }
